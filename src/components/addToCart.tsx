2b6ab38db4b932c781a3f5ddb02a1bc69c3ecc02
ABigJuicyBurger <70484524+ABigJuicyBurger@users.noreply.github.com>
2024-11-29 11:16:00 -0700

    changed addtocart to handle adding and removing quantity

--- src/components/addToCart.tsx
+++ src/components/addToCart.tsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
+
 export default function AddToCart() {
+  const [quantity, setQuantity] = useState(1);
+
+  const increaseNumber = () => {
+    setQuantity((prev) => prev + 1);
+  };
+  const decreaseNumber = () => {
+    setQuantity((prev) => prev - 1);
+  };
+
   return (
     <div>
-      <button>-</button>
-      <input placeholder="1"></input>
-      <button>+</button>
+      <button onClick={decreaseNumber}>-</button>
+      <input
+        type="number"
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+        placeholder="1"
+      ></input>
+      <button onClick={increaseNumber}>+</button>
       <button>Add to Cart</button>
     </div>
   );
